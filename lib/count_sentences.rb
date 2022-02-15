bddd4b16130587c549a6e3890b1ad9e7614be66c
Read Langworthy <rlangworthy@gmail.com>
2022-02-15 14:38:26 -0800

    commit tree a8a2972134aa22576b02844f1af961bb609fedbc

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,42 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    p self.split(/[.!?]/)
+    self.split(/[.!?]\s/).length
   end
 end
+
+
+class Dog
+  attr_accessor :name, :owner
+
+  def initialize(name)
+    @name = name
+  end
+
+  def bark
+    puts "Woof!"
+  end
+
+  def get_adopted(owner_name)
+    self.owner = owner_name
+  end
+
+end
+
+
+binding.pry
